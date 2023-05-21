b95b2386fb9b828243a03665a3ca77ee1510e5c3
kuzhelga <kuzhelga81@gmail.com>
2023-05-21 15:49:48 +0300

    add codes to create 3 tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id smallint PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	contact_name varchar(100) NOT NULL,
+	employee_id smallint REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
